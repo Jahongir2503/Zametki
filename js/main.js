27adabbe87b87917e6d8b7d3fd02e244275933b2
mirzo <jojo.isiomidinov@gmail.com>
2024-02-01 17:07:22 +0500

    new begin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 new Vue({
     el: '#app',
     data: {
+        firstColumn: [],
+        secondColumn: [],
+        thirdColumn: [],
+        note: {
+            eventName: '',
+            taskName: ''
+        },
+        error: '',
 
     },
+    methods: {
+        addNote() {
+            if (this.note.eventName === '') {
+                this.error = 'The name of the Note has not been entered';
+                return;
+            } else if (this.note.taskName === '') {
+                this.error = 'Enter the task!';
+                return;
+            }
+            this.error = '';
+
+            this.firstColumn.push({
+                event: this.note.eventName,
+                task: this.note.taskName
+            })
+        }
+
+    }
 });
